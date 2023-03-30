202cb716c6b0a8fda04e79b7fc832910e91815b6
nazar15yurchuk <nazaryurchuk15@gmail.com>
2023-03-31 02:29:34 +0300

    lesson_11

--- dist/app.js
+++ dist/app.js
@@ -29,18 +29,50 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = require("dotenv");
 const express_1 = __importDefault(require("express"));
 const express_fileupload_1 = __importDefault(require("express-fileupload"));
+const http = __importStar(require("http"));
 const mongoose = __importStar(require("mongoose"));
+const socket_io_1 = require("socket.io");
+const swaggerUi = __importStar(require("swagger-ui-express"));
+const swaggerJson = __importStar(require("./utils/swagger.json"));
 (0, dotenv_1.config)();
 const configs_1 = require("./configs");
 const crons_1 = require("./crons");
 const routers_1 = require("./routers");
 const app = (0, express_1.default)();
+const server = http.createServer(app);
+const io = new socket_io_1.Server(server, {
+    cors: {
+        origin: "http://localhost:63342/",
+    },
+});
+io.on("connection", (socket) => {
+    console.log(socket.id);
+    socket.on("message:send", (text) => {
+        io.emit("message:get", `${text}`);
+    });
+    socket.on("disconnect", () => {
+        console.log(`${socket.id} disconnected`);
+    });
+    socket.on("join:room", ({ roomId }) => {
+        socket.join(roomId);
+        socket
+            .to(roomId)
+            .emit("user:joined", { socketId: socket.id, action: "Joined" });
+        socket.on("leave:room", ({ roomId }) => {
+            socket.leave(roomId);
+            socket
+                .to(roomId)
+                .emit("user:left", { socketId: socket.id, action: "Left" });
+        });
+    });
+});
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use((0, express_fileupload_1.default)());
 app.use("/users", routers_1.userRouter);
 app.use("/cars", routers_1.carRouter);
 app.use("/auth", routers_1.authRouter);
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerJson));
 app.use((err, req, res, next) => {
     const status = err.status || 500;
     return res.status(status).json({
@@ -49,7 +81,7 @@ app.use((err, req, res, next) => {
     });
 });
 console.log(process.env.PORT);
-app.listen(configs_1.configs.PORT, async () => {
+server.listen(configs_1.configs.PORT, async () => {
     await mongoose.connect(configs_1.configs.DB_URL).then();
     (0, crons_1.cronRunner)();
     console.log(`Server has started on PORT ${configs_1.configs.PORT}`);
--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,57 @@
 import { config } from "dotenv";
-import express, { NextFunction, Request, Response } from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
 import fileUploader from "express-fileupload";
+import * as http from "http";
 import * as mongoose from "mongoose";
+import { Server, Socket } from "socket.io";
+import * as swaggerUi from "swagger-ui-express";
+
+import * as swaggerJson from "./utils/swagger.json";
 
 config();
+
 import { configs } from "./configs";
 import { cronRunner } from "./crons";
 import { ApiError } from "./errors";
 import { authRouter, carRouter, userRouter } from "./routers";
 
-const app = express();
+const app: Application = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:63342/",
+  },
+});
+
+io.on("connection", (socket: Socket) => {
+  console.log(socket.id);
+  // io.emit("user:connected", { message: "USER CONNECTED" });
+  // socket.broadcast.emit("user:connected", { message: "USER CONNECTED" });
+
+  socket.on("message:send", (text) => {
+    io.emit("message:get", `${text}`);
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`${socket.id} disconnected`);
+  });
+
+  socket.on("join:room", ({ roomId }) => {
+    socket.join(roomId);
+
+    socket
+      .to(roomId)
+      .emit("user:joined", { socketId: socket.id, action: "Joined" });
+
+    socket.on("leave:room", ({ roomId }) => {
+      socket.leave(roomId);
+      socket
+        .to(roomId)
+        .emit("user:left", { socketId: socket.id, action: "Left" });
+    });
+  });
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -18,6 +60,7 @@ app.use(fileUploader());
 app.use("/users", userRouter);
 app.use("/cars", carRouter);
 app.use("/auth", authRouter);
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerJson));
 
 app.use((err: ApiError, req: Request, res: Response, next: NextFunction) => {
   const status = err.status || 500;
@@ -29,7 +72,7 @@ app.use((err: ApiError, req: Request, res: Response, next: NextFunction) => {
 
 console.log(process.env.PORT);
 
-app.listen(configs.PORT, async () => {
+server.listen(configs.PORT, async () => {
   await mongoose.connect(configs.DB_URL).then();
   cronRunner();
   console.log(`Server has started on PORT ${configs.PORT}`);
